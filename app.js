b4584fb2bc60be3dc236c904a5667d410cae6ce2
gapasquantonio <pasquantonioo@gmail.com>
2023-12-17 22:27:28 -0300

    feat: add body parser

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-
 const express = require("express")
 
 const app = express()
-app.use((req, res, next) => {
-    console.log('In the middleware!')
-    next()
+app.use()
+
+
+
+app.use('/add-product', (req, res) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Procuct</button></input></form>')
+});
+app.use('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect("/")
+
 })
-app.use((req, res, next) => {
-    console.log('In the another middleware!')
+app.use('/', (req, res) => {
     res.send('<h1>Hello from Express!!</h1>')
 
 })
-const server = http.createServer(app);
 
-server.listen(3000)
+app.listen(3000)
