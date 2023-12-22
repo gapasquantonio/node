756266e4554f1837853dfd69cff92a6239a134c2
gapasquantonio <pasquantonioo@gmail.com>
2023-12-21 22:20:11 -0300

    feat: finish module

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const path = require("path")
 const express = require("express")
 const bodyParser = require('body-parser')
 
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const app = express()
+
+app.set('view engine', 'pug')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
 
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,19 @@ const express = require('express')
 const rootDir = require('../util/path')
 const router = express.Router()
 
+const products = [];
+
 router.get('/add-product', (req, res) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
-router.post('/product', (req, res, next) => {
-    console.log(req.body)
+router.post('/add-product', (req, res, next) => {
+    products.push({
+        title: req.body.title
+    })
     res.redirect("/")
 
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
